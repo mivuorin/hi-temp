c8aa737c92060ce8cc37397b5c3bf992689b9ea7
mivuorin <mikko.h.vuorinen@gmail.com>
2024-03-09 12:51:21 +0200

    HiTemp.Client which sends demo messages to event hub.

--- HiTemp/HiTemp.Client/Program.cs
+++ HiTemp/HiTemp.Client/Program.cs
@@ -1,3 +1,25 @@
-﻿throw new NotImplementedException();
+﻿using Azure.Identity;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+using System.Text;
 
+var eventHubNamespace = "hitemp.servicebus.windows.net";
+var eventHubName = "hitemphub";
+var credential = new DefaultAzureCredential();
 
+await using var client = new EventHubProducerClient(eventHubNamespace, eventHubName, credential);
+
+using var batch = await client.CreateBatchAsync();
+
+for (int i = 1; i < 4; i++)
+{
+    var bytes = Encoding.UTF8.GetBytes($"Event {i}");
+    var data = new EventData(bytes);
+
+    // TODO Check error if batch size is too big. Now just ignore it.
+    batch.TryAdd(data);
+}
+
+await client.SendAsync(batch);
+
+Console.WriteLine("Published 3 events");
